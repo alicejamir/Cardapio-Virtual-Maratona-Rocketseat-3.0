996454ceda0a62f4248c38145924da968fa84ccb
alicejamir <105648290+alicejamir@users.noreply.github.com>
2022-08-11 19:45:32 -0300

    Atualizaçoes de JS e CSS

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+/*  abre e fecha o menu quando clicar no icone:hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
 /*Botao voltar para o topo*/
 const backToTopButton = document.querySelector('.back-to-top')
 window.addEventListener('scroll', function () {
-  if (window.scrollY >= 180) {
+  if (window.scrollY >= 150) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
